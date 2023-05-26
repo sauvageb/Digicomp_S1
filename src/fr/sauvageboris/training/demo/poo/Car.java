154c1adcda10320f2cbf94ae0371a2458e269701
sauvageb <sauvageboris.pro@gmail.com>
2023-05-26 12:23:11 +0200

    demo : update demo using equals

--- src/fr/sauvageboris/training/demo/poo/Car.java
+++ src/fr/sauvageboris/training/demo/poo/Car.java
@@ -1,5 +1,7 @@
 package fr.sauvageboris.training.demo.poo;
 
+import java.util.Objects;
+
 public class Car {
     // Attributs
     String color;
@@ -12,5 +14,28 @@ public class Car {
         this.brand = brand;
         this.model = model;
         this.nbWheels = nbWheels;
+    }
+
+    @Override
+    public boolean equals(Object objectCompared) {
+        if (this == objectCompared) return true;
+        if (objectCompared == null || this.getClass() != objectCompared.getClass()) return false;
+        Car car = (Car) objectCompared;
+        return nbWheels == car.nbWheels && Objects.equals(color, car.color) && Objects.equals(brand, car.brand) && Objects.equals(model, car.model);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(color, brand, model, nbWheels);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "color='" + color + '\'' +
+                ", brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", nbWheels=" + nbWheels +
+                '}';
     }
 }
--- src/fr/sauvageboris/training/demo/poo/Demo.java
+++ src/fr/sauvageboris/training/demo/poo/Demo.java
@@ -5,9 +5,15 @@ public class Demo {
     public static void main(String... arguments) {
         System.out.println("Démo POO");
 
-        Car car1 = new Car(arguments[0], arguments[1], arguments[2], 4);
+        Car car1 = new Car("Red", "Peugeot", "208", 4);
+        Car car2 = new Car("Red", "Peugeot", "208", 4);
         System.out.println(car1.color);
         System.out.println(car1.brand);
         System.out.println(car1.model);
+
+        System.out.println(car1.equals(car2));
+
+
+
     }
 }
